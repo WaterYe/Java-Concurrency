f4d873a8cae7f3ec16f70bb56e07cd171819ba36
Yao Wang <water4ye@gmail.com>
2020-03-10 01:03:01 -0700

    concurrency Synchronized

---
+++ concurrency/src/SynchronizedEvenGenerator.java
@@ -0,0 +1,13 @@
+public class SynchronizedEvenGenerator extends IntGenerator {
+    private int currentValue = 0;
+    @Override
+    public synchronized int next() {
+        currentValue++;
+        Thread.yield();
+        currentValue++;
+        return currentValue;
+    }
+    public static void main(String[] args) {
+        EvenChecker.test(new SynchronizedEvenGenerator());
+    }
+}
